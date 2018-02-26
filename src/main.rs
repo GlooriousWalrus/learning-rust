d6a4eef64224a152fc3f1db7cb2d2660785000de
GlooriousWalrus <GlooriousWalrus@github.com>
2018-02-26 17:12:18 +0200

    stepping on some rakes while learning.

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,16 @@ extern crate rand;
 #[macro_use]
 extern crate rand_derive;
 
+use rand::Rng;
+use std::vec::Vec;
+use std::{thread, time};
 
+//create a constant array of names that will have a static lifetime
+const NAMESLIST: [&'static str; 20] = ["Daedalus", "Icaros", "Ares", "Dionysus", "Hades", "Hephaestus",
+							   "Zeus", "Chronos", "Thanatos", "Atlas", "Talos", "Typhon",
+							   "Aergia", "Caerus", "Odysseus", "Kratos", "Prometheus", "Proteus",
+							   "Helios", "Asclepius"
+							  ];
 
 #[derive(Debug, Rand)]
 enum Sex { // 50% creation chance
@@ -21,105 +30,107 @@ enum Color { //inherited from mother object
 	Spotted
 }
 
-use rand::Rng;
-use std::vec::Vec;
-use std::{thread, time};
-
 struct Bunny<'a> { //lifetime declared as a to not outlive the name
 
 	sex: Sex, //50% creation chance
 	color: Color, //inherited from parents
 	name: &'a str, // randomly chosen from list, lifetime declared as a
 	age: u64,
-	radioactive_mutant_vampire_bunny: bool //probability of true is 2% on creation
+	ghoul: bool //probability of true is 2% on creation
 }
 
 impl<'a> Bunny<'a> {
     fn announcebirth(&self) {
 
-        println!("Bunny {} was born!" ,self.name);
-        dosleep(&1);
+        println!("Bunny {} was born!" ,&self.name);
+        dosleep(1);
 
     }
 
-    fn die(&self) {
+	fn incrementage(&mut self) {
 
-        if &self.radioactive_mutant_vampire_bunny == &false {
+		self.age += 1; //increment age
 
-            if &self.age > &10 {
-
-                println!("Bunny {} has died!" ,self.name);
-                dosleep(&1);
-                drop(&self); //destruct the object and delete it
-
-        } else {
-
-            if &self.age > &50 {
-
-                println!("Radioactive Mutant Vampire Bunny {} has died!" ,self.name);
-                dosleep(&1);
-                drop(&self); //destruct the object and delete it
-
-                }
-            }
-        }
-    }
+			// old age catches up
+			if self.ghoul == true {
+				if self.age > 50 {
+					println!("{} has died!", self.name);
+					drop(self);
+				}
+			} else {
+				if self.age > 10 {
+					println!("{} has died!", self.name);
+					drop(self);
+				}
+			}
+	}
 }
 
 
-fn dosleep(time: &u64) {
+fn dosleep(time: u64) {
 
-    thread::sleep(time::Duration::from_secs(*time));
+    thread::sleep(time::Duration::from_secs(time));
+
+}
+
+fn gameloop(bunnies: &mut Vec<Bunny>, names2: [&'static str; 20]) {
+
+	let mut rng = rand::thread_rng();
+
+	for x in 0..5 { //looping through vector of Bunny structs 5 times. each iteration pushes a Bunny struct with populated fields to the vector bunnies.
+		&bunnies.push( Bunny { sex: rng.gen(),
+							  color: rng.gen(),
+							  name: rng.choose(&names2).unwrap(), // https://habrahabr.ru/post/274485/
+							  age: 0,
+							  ghoul: false
+						  });
+		&bunnies[x].announcebirth();
+	}
+
+	// for bunny in bunnies {
+	//
+	// 	println!("DEBUG: {} is constructed in gameloop. sex: {:?}, color: {:?}, age: {}", bunny.name, bunny.sex, bunny.color, bunny.age);
+	//
+	// }
+
+	let mut turn: u64 = 1;
+
+	loop {
+
+		println!("Beginning of turn {}", &turn);
+
+		for bunny in bunnies.iter() {
+
+			// need to figure out how to make it work...
+			bunny.incrementage();
+
+		}
+
+		turn += 1;
+	}
+
+}
+
+fn greet() {
+
+	println!("///////////////graduation in rust////////////////////");
 
 }
 
 fn main() {
 
-    let listofnames: [&str; 20] = ["Daedalus", "Icaros", "Ares", "Dionysus", "Hades", "Hephaestus",
-                                   "Zeus", "Chronos", "Thanatos", "Atlas", "Talos", "Typhon",
-                                   "Aergia", "Caerus", "Odysseus", "Kratos", "Prometheus", "Proteus",
-                                   "Helios", "Asclepius"
-                                  ];
+	greet();
 
-    println!("///////////////graduation in rust////////////////////");
-    let mut rng = rand::thread_rng();
-    let mut bunnies = Vec::new();
+    let mut bunnies: Vec<Bunny> = Vec::new(); // declaring a mutable vector of structs called bunnies
+	gameloop(&mut bunnies, NAMESLIST); // wtf is going on?
 
-    for _x in 0..5 {
-        bunnies.push( Bunny { sex: rng.gen(),
-                              color: rng.gen(),
-                              name: rng.choose(&listofnames).unwrap(), // https://habrahabr.ru/post/274485/
-                              age: 0,
-                              radioactive_mutant_vampire_bunny: false
-                            })
-    }
+	println!("MAIN FUNCTION");
 
+	for bunny in bunnies {
 
-    let mut turn: u64 = 0;
+		println!("DEBUG: {} in main scope", bunny.name);
 
-    loop { //main loop
+	}
 
-        println!("new turn nr. {} has begun!", &turn);
-        dosleep(&1);
-
-        for bunny in bunnies {
-
-            bunny.die();
-            bunny.announcebirth();
-
-        }
-
-        if  &bunnies.is_empty() == &true {
-
-            println!("All bunnies have died, the turn is {}. program will now terminate!", &turn);
-            dosleep(&1);
-            break;
-
-        }
-
-        turn += 1;
-        dosleep(&2);
-    }
-
-
+	println!("PROGRAM TERMINATED");
 }
